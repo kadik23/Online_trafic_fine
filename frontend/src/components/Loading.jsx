9bbb38e0d7025a1ada049b76a70ac0ee6f4fcee6
unknown <kadiksalah03@gmail.com>
2024-03-27 17:42:16 +0100

    remove node_modules

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+    return (
+        <div  className='w-screen h-screen flex justify-center items-center'>
+            <div class="custom-loader"></div>
+        </div>
+    )
+}
+
+export default Loading
